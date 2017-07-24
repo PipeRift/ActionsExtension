90981a5b35f115363cc9966893e18de633f42728
muit <muit@piperift.com>
2017-07-24 13:38:48 +0200

    Task Deprecation support

---
+++ Source/AIExtension/Private/Tasks/Task.cpp
@@ -0,0 +1,9 @@
+// Copyright 2015-2017 Piperift. All Rights Reserved.
+
+#include "AIExtensionPrivatePCH.h"
+#include "Task.h"
+
+UDEPRECATED_Task::UDEPRECATED_Task(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+}
