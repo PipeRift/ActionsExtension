aa265ed9a4f958ba3947e46f08d594dc58238d57
muit <muit@piperift.com>
2017-08-16 11:44:13 +0200

    Removed Deprecated Tasks

--- Source/AIExtension/Private/Tasks/Task.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2015-2017 Piperift. All Rights Reserved.
-
-#include "AIExtensionPrivatePCH.h"
-#include "Task.h"
-
-UDEPRECATED_Task::UDEPRECATED_Task(const FObjectInitializer& ObjectInitializer)
-    : Super(ObjectInitializer)
-{
-}
